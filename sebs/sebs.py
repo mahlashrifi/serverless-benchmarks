7813303592c24a3f44c7d642e492efe2d9d198b7
prajinkhadka <prawjeenkhadka@gmail.com>
2024-06-24 22:02:35 -0500

    Black fomatter

--- sebs/sebs.py
+++ sebs/sebs.py
@@ -85,7 +85,7 @@ class SeBS(LoggingBase):
         logging_filename: Optional[str] = None,
         deployment_config: Optional[Config] = None,
     ) -> FaaSSystem:
-        dep_config = config['deployment']
+        dep_config = config["deployment"]
         name = dep_config["name"]
         implementations: Dict[str, Type[FaaSSystem]] = {"local": Local}
 
@@ -107,14 +107,13 @@ class SeBS(LoggingBase):
             implementations["openwhisk"] = OpenWhisk
 
         if name not in implementations:
-            raise RuntimeError("Deployment {name} not supported!".format(name=name)) 
+            raise RuntimeError("Deployment {name} not supported!".format(name=name))
 
-        if (
-            (config['experiments']['container_deployment']) and 
-            (name not in self._config.supported_container_deployment())
-            ):
-            raise RuntimeError("Container deployment is not supported in {name}.".format(name=name)) 
-            
+        if (config["experiments"]["container_deployment"]) and (
+            name not in self._config.supported_container_deployment()
+        ):
+            raise RuntimeError("Container deployment is not supported in {name}.".format(name=name))
+
         # FIXME: future annotations, requires Python 3.7+
         handlers = self.generate_logging_handlers(logging_filename)
         if not deployment_config:
